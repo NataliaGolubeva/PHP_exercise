659e846fcc313402f8e65974e8d5f84e2ea04262
NataliaGolubeva <nataliagol787@gmail.com>
2020-11-26 22:51:18 +0100

    Student to table

---
+++ Exercises/OEF_1.2/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Bootstrap Example</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+    <h1>Steden die je moet bezoeken</h1>
+    <p>Kies jow bestemming!</p>
+</div>
+
+<div class="container">
+    <div class="row">
+        <?php
+        $images = array("london", "paris", "berlin");
+        $names = array("Londen", "Parijs", "Berlijn");
+
+        function columns($imgcity, $imgname) {
+            $column = 0;
+        foreach(array_combine($imgcity, $imgname) as $image => $name) {
+            $column = $column + 1;
+         echo "<div class='col-sm-4'>";
+            echo "<h3>$name</h3>";
+            echo "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit...</p>";
+            echo "<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...</p>";
+        echo "<img width='auto' height='180px' src='img/$image.jpg'> <br>";
+        echo "</div>";
+        }}
+        echo columns($images, $names)
+        ?>
+
+</body>
+</html>
+
